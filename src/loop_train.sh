d8dc76cbd32ab8e2dd38ac2f9dc0a3d3570c6891
paolo-gajo <paolo.gajo2@unibo.it>
2025-04-01 05:19:07 +0000

    push model names

--- src/loop_train.sh
+++ src/loop_train.sh
@@ -2,7 +2,7 @@
 #SBATCH -J train_kgc
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:h100:1
 #SBATCH --time=24:00:00
 #SBATCH --mem=64G
 #SBATCH --output=./.slurm/%j_output.log
